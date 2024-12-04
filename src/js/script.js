950d350ea268990f6e5cd6582300bad22c41a5d0
aymonier-elias <elias.aymonier@edu.univ-fcomte.fr>
2024-12-04 10:26:51 +0100

    Mise en place de l'ouverture fermeture de lightbow

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
 // *** LIGHTBOX ***
+
+const img = document.querySelectorAll("img");
+const lightbox = document.querySelector("#lightbox");
+
+img.forEach(element => {
+    element.addEventListener('click', () => {
+        lightbox.showModal();
+    })
+});
+
+lightbox.addEventListener('click', () => {
+    lightbox.close();
+})
