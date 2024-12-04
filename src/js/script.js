37d2ebd7d3fb94aba30fde47c17cf4527ec45e27
aymonier-elias <elias.aymonier@edu.univ-fcomte.fr>
2024-12-04 10:36:14 +0100

    Mise en place du changement d'image en fonction de l'image clicker, en utilisant dataset

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,11 @@
 
 const img = document.querySelectorAll("img");
 const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector(".lightbox__img")
 
 img.forEach(element => {
     element.addEventListener('click', () => {
+        lightboxImg.src = element.dataset.fullImg;
         lightbox.showModal();
     })
 });
